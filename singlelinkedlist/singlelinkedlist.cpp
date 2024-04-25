dd33af069dfa9f9ef9bd604583ca55e071d951d4
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2024-04-25 10:23:50 +0700

    add node 1

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -7,6 +7,19 @@ struct Node {
     Node* next;
 };
 
+Node* START = NULL;
+
+void addNode() {
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
+}
 
 int main()
 {
